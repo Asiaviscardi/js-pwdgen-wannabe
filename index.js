e8d0f3839456ffabb5072ad02088906fb31fd301
Asiaviscardi <asia.viscardii@gmail.com>
2023-09-06 16:56:23 +0200

    termine esercizio

---
+++ index.js
@@ -0,0 +1,17 @@
+const nome = prompt("come ti chiami?")
+
+const cognome = prompt("Qual è il tuo cognome?")
+
+const colore = prompt("Qual è il tuo colore preferito?")
+
+const number = 23;
+
+const password = nome + cognome + colore + number
+
+const passwordDiv = document.getElementById('password');
+
+passwordDiv.innerHTML = password
+
+
+
+
